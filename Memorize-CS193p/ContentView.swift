ef47b3819271a1fa67e2229112fd11b8b5272f42
Baris <karalarbaris@gmail.com>
2021-09-06 13:07:05 +0200

    Initial Commit

---
+++ Memorize-CS193p/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize-CS193p
+//
+//  Created by Baris Karalar on 6.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize-CS193p/Memorize_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_CS193pApp.swift
+//  Memorize-CS193p
+//
+//  Created by Baris Karalar on 6.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
